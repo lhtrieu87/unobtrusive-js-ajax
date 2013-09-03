499921fb45d5d69a387d82ca1bf4467f361dc66b
darksteelgames <darksteelgames@darksteelgamess-Mac-mini.local>
2013-09-03 21:16:51 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnobtrusiveJsAjax::Application.config.secret_key_base = '646e6b46cfc9755d5bfc035965537b286f9a97aac82d1bbd25a0fc9a621a73af123ca17cd0d1db5b83106cc1cf16034eee11fecb04f51f96c2469f1b799cd8fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UnobtrusiveJsAjax::Application.config.session_store :cookie_store, key: '_unobtrusive-js-ajax_session'
